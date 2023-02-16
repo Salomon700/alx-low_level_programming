1a2d5537d486531c1ad22744fc74b3eacf926fd3
Sol <Sol@github.com>
2023-02-16 10:14:02 -0800

    fixed 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main-This is the main fuction description
+ *
+ *Return: 0 for succesful exit
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main-This is the main fuction description
+ *
+ *Return: 0 for succesful exit
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
