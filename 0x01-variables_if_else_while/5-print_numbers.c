4d96c001eebbedf8c44475611a021162d1684793
Sol <Sol@github.com>
2023-02-19 01:39:17 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function to print all single digits of base 10
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int up = 1;
+
+	while (up < 10)
+	{i
+		printf("%d", up);
+		up++;
+	}
+	printf("\n");
+	return (0);
+}
