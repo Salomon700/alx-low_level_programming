d4f6579c3d141ff8790f5d26c98bfba175feaa7f
Sol <Sol@github.com>
2023-02-21 09:08:12 -0800

    task 0 and 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet
+ *
+ * Return:0 on success
+ */
+
+void print_alphabet(void)
+{
+	a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
