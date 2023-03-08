70b9758b310644098c247f4cd2355ca73b99cd7c
root <root@SOL>
2023-03-09 00:34:08 +0200

    fixed task 0 1 and 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strlen_recursion - function that returns 
+* _strlen_recursion - function that returns
 * the length of a string
 * @s: string measured
 *Return: length of the string
