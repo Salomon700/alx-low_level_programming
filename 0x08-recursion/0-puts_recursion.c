946b01799d6dd4321ee63d069b4cc4ee1f73b683
root <root@SOL>
2023-03-09 00:25:43 +0200

    task 0,1 and 2

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: character printed
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s +1);
+	}
+}
+
