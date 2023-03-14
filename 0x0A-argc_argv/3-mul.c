678b5646fd8947c6ac7c1009e3896dc7c0e4be66
root <root@SOL>
2023-03-14 19:46:07 +0200

    task 3 4 and 5 on argv and arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - program that multiplies 2 numbers
- * @argv: array of pointers for the arguments
- * @argc: count of arguments
- * Return: return 1 if there are no 2 arguments
+ * main - prints the product of two integers to the stdout
+ * @argc: the number of passed arguments
+ * @argv: the array of passed arguments
+ *
+ * Return: the product if argc == 2, else 1
  */
-
 int main(int argc, char *argv[])
 {
-	int numb1, numb2, result;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	numb1 = atoi(argv[1]);
-	numb2 = atoi(argv[2]);
-	result = numb1 * numb2;
-
-	printf("%d\n", result);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
