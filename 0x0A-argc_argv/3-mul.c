1a0d1d7b0950355fe8ea4938ad22245e0458b6e6
Salomon700 <ntwalisolomon9@gmail.com>
2024-01-27 15:46:44 +0200

    multiplication of arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies 2 arguments
+ *
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", num1 * num2);
+
+	return 0;
+}
